79cadc6843df830e8a1c6743b488a3f47c9c1f6e
DavidDiaz1949 <david.diaz-d@mail.escuelaing.edu.co>
2020-08-24 22:43:29 -0500

    inicio shopcar

---
+++ src/components/Carrito/Carrito.js
@@ -0,0 +1,34 @@
+import React from 'react';
+
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import TableCell from '@material-ui/core/TableCell';
+import Checkbox from '@material-ui/core/Checkbox';
+import dino from '../../img/dino.jpg';
+import "./Carrito.css";
+export class Carrito extends React.Component{
+
+    constructor(props) {
+        super(props);
+    }
+
+    render(){
+
+        return(
+            <TableHead>
+                <TableRow>
+                    <TableCell>
+                        <Checkbox
+                        />
+                    </TableCell>
+                    <TableCell>
+                        <img src={dino} className="ima-dino"/>
+                    </TableCell>
+                </TableRow>
+            </TableHead>
+        );
+    }
+}
+
+
+export default Carrito;
