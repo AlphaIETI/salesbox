f94cbeee9038e6f9b96e0a88e70aacaefcc78a77
DavidDiaz1949 <david.diaz-d@mail.escuelaing.edu.co>
2020-08-25 22:10:50 -0500

    Avance tablas del carrito de compras

--- src/components/Carrito/Carrito.js
+++ src/components/Carrito/Carrito.js
@@ -1,10 +1,12 @@
 import React from 'react';
 
-import TableHead from '@material-ui/core/TableHead';
-import TableRow from '@material-ui/core/TableRow';
+import th from '@material-ui/core/TableHead';
+import tr from '@material-ui/core/TableRow';
 import TableCell from '@material-ui/core/TableCell';
 import Checkbox from '@material-ui/core/Checkbox';
 import dino from '../../img/dino.jpg';
+import dino2 from '../../img/dino2.jpg';
+import dino3 from '../../img/dino3.jpg';
 import "./Carrito.css";
 export class Carrito extends React.Component{
 
@@ -15,24 +17,52 @@ export class Carrito extends React.Component{
     render(){
 
         return(
-            <TableHead>
-                <TableRow>
-                    <TableCell>
-                        <Checkbox
-                        />
-                    </TableCell>
-                    <TableCell>
-                        <img src={dino} className="ima-dino"/>
-                    </TableCell>
-                    <TableCell>
-                        <img src={dino} className="ima-dino"/>
-                    </TableCell>
-                    <TableCell>
-                        <img src={dino} className="ima-dino"/>
-                    </TableCell>
-
-                </TableRow>
-            </TableHead>
+            <table className="tabla">
+                <tbody className="body">
+                    <tr>
+                        <td className="columnaCheckBox">
+                            <Checkbox/>
+                        </td>
+                        <td className="columnaImagen">
+                            <img src={dino} className="ima-dino"/>
+                        </td>
+                        <td className="columnaTexto">
+                            audifonos inalambricos para jugar los mejores juegos que existen dfgdfgdsf
+                        </td>
+                        <td className="columnaPrecio">
+                            $37.000
+                        </td>
+                    </tr>
+                    <tr>
+                        <td className="columnaCheckBox">
+                            <Checkbox/>
+                        </td>
+                        <td className="columnaImagen">
+                            <img src={dino2} className="ima-dino"/>
+                        </td>
+                        <td className="columnaTexto">
+                            audifonos inalambricos para jugar los mejores juegos que existen dfgdfgdsf
+                        </td>
+                        <td className="columnaPrecio">
+                            $317.000
+                        </td>
+                    </tr>
+                    <tr>
+                        <td className="columnaCheckBox">
+                            <Checkbox/>
+                        </td>
+                        <td className="columnaImagen">
+                            <img src={dino3} className="ima-dino"/>
+                        </td>
+                        <td className="columnaTexto">
+                            audifonos inalambricos para jugar los mejores juegos que existen dfgdfgdsf
+                        </td>
+                        <td className="columnaPrecio">
+                            $37.000
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
         );
     }
 }
