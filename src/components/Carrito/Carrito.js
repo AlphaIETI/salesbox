f4da56e29194a721a46745d737c0aef896e98a7a
DavidDiaz1949 <david.diaz-d@mail.escuelaing.edu.co>
2020-09-02 12:03:14 -0500

    mejoras

--- src/components/Carrito/Carrito.js
+++ src/components/Carrito/Carrito.js
@@ -4,8 +4,8 @@ import th from '@material-ui/core/TableHead';
 import tr from '@material-ui/core/TableRow';
 import TableCell from '@material-ui/core/TableCell';
 import Checkbox from '@material-ui/core/Checkbox';
-import dino from '../../img/dino.jpg';
-import dino2 from '../../img/dino2.jpg';
+import saco from '../../img/saco.jpg';
+import saco2 from '../../img/saco2.jpg';
 import dino3 from '../../img/dino3.jpg';
 import "./Carrito.css";
 export class Carrito extends React.Component{
@@ -16,39 +16,45 @@ export class Carrito extends React.Component{
 
     render(){
 
+
         return(
             <div className="tabla">
                 <div className="main">
                     <Checkbox color="default"></Checkbox>
                 </div>
                 <div className="ImageColumn">
-                    <img src={dino} className="ima-dino"/>
+                    <img src={saco} className="ima-dino"/>
                 </div>
                 <div className="DescriptionColumn">
                     <div className="DescriptionColumnText" >
                         Buzo Buso Ancho Hoodie Mujer Stranger Things
                     </div>
                     <div className="DescriptionColumnText">
-                        hola como vas
+                        Solo quedan 5 unidades en Stock
                     </div>
                     <div className="DescriptionColumnText">
-                        no muy bien gracias
+                        Cantidad: 5
                     </div>
                     <div className="DescriptionColumnItems">
                         <div className="ItemColumn">
-                            hola
+                            Eliminar
                         </div>
                         <div className="ItemColumn">
-                            Como
+                            Mas informacion
                         </div>
                         <div className="ItemColumn">
-                            estas
+                            comparar
                         </div>
                     </div>
 
                 </div>
                 <div className="PriceColumn">
-                    37.500
+                    <div>
+                        Precio
+                    </div>
+                    <div>
+                        37.500
+                    </div>
                 </div>
             </div>
         );
