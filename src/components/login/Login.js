851460656139bff7d52b13743eb5071ca79499c7
checho1998 <c.hecho.28@hotmail.com>
2020-10-29 00:06:49 -0500

    login de Cliente

--- src/components/login/Login.js
+++ src/components/login/Login.js
@@ -82,6 +82,19 @@ export default function Login() {
     alert("No está registrado");
   };
 
+  const handleOnClickClient = () => {
+    if (document.getElementById("email").value === ""
+    || document.getElementById("contrasenaCliente").value === ""
+    ){
+      alert("Alguno de los campos esta vacio")
+    }
+    else{
+      let client =
+          {email:document.getElementById("email").value, password:document.getElementById("contrasenaCliente").value}
+      loginClient(client);
+
+    }
+    };
 
   const handleOnClickEntity=() =>{
     let entity={email:document.getElementById("emailEmpresa").value, password:document.getElementById("contrasenaEmpresa").value}
@@ -112,8 +125,9 @@ export default function Login() {
         response.json().then(function (res) {
           console.log(res);
           localStorage.setItem('isAdmin',true);
+          localStorage.setItem('nameEntity',res.name);
+          localStorage.setItem('isLoggedIn',true);
           setIsLoggedIn(true);
-        
         })
       } else {
         console.log("");
@@ -126,9 +140,36 @@ export default function Login() {
 
   }
 
+  const loginClient = (client)=>{
+    fetch('https://salesbox-alpha-backend.herokuapp.com/clients/email/'+client.email,{
+    //fetch('http://localhost:8080/clients/email/'+client.email,{
+      method: 'GET'
+    }).then(function(response){
+      //console.log(response.ok)
+     if (response.ok){
 
+       response.json().then(function (res) {
 
+         //localStorage.setItem(,true);
+         if(client.email === res.email && client.password === res.password){
+           
+           localStorage.setItem('isLoggedIn',true);
+           window.location='/Home';
+           setIsLoggedIn(true);
+         }
+     });
+     }
+     else{
+       console.log("");
 
+       alert("Usuario o Contraseña Incorrecto");
+     }
+    }).catch(function (error) {
+
+      console.log("Bad petition:" + error.message);
+
+    });
+  }
 
 
 
@@ -214,7 +255,7 @@ export default function Login() {
         />
         <br />
         <br />
-        <button className="myButton2"  style={{ position: 'relative', top: '0px', right: '-140px' }}>
+        <button className="myButton2" onClick={handleOnClickClient} style={{ position: 'relative', top: '0px', right: '-140px' }}>
           Iniciar Sesión
           </button>
         <br>
