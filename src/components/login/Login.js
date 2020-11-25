ddcf6721c3b48b06a3e8226ad4aa1df64d491e4c
julianagarzond <juliana.garzon-d@mail.escuelaing.edu.co>
2020-11-24 22:42:16 -0500

    nuevo login

--- src/components/login/Login.js
+++ src/components/login/Login.js
@@ -1,15 +1,26 @@
 import React, { useState } from 'react';
-import PropTypes from 'prop-types';
-import { makeStyles } from '@material-ui/core/styles';
-import AppBar from '@material-ui/core/AppBar';
-import Tabs from '@material-ui/core/Tabs';
-import Tab from '@material-ui/core/Tab';
-import Typography from '@material-ui/core/Typography';
-import Box from '@material-ui/core/Box';
-import logo from '../../logo.png';
-import { Redirect, Route } from 'react-router-dom';
+import Avatar from '@material-ui/core/Avatar';
+import Button from '@material-ui/core/Button';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import TextField from '@material-ui/core/TextField';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Checkbox from '@material-ui/core/Checkbox';
 import GoogleLogin from 'react-google-login';
 import { Link } from 'react-router-dom';
+import Paper from '@material-ui/core/Paper';
+import Box from '@material-ui/core/Box';
+import Grid from '@material-ui/core/Grid';
+import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Input from '@material-ui/core/Input';
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
+import PropTypes from 'prop-types';
+import FaceIcon from '@material-ui/icons/Face';
+import HomeWorkIcon from '@material-ui/icons/HomeWork';
+import { Redirect, Route } from 'react-router-dom';
 
 function TabPanel(props) {
   const { children, value, index, ...other } = props;
@@ -44,26 +55,51 @@ function a11yProps(index) {
   };
 }
 
+
+
 const useStyles = makeStyles((theme) => ({
   root: {
-    flexGrow: 1,
-    backgroundColor: theme.palette.background.paper,
+    height: '100vh',
+    display: 'flex',
+  },
+  image: {
+    backgroundImage: 'url(https://res.cloudinary.com/deavblstk/image/upload/v1606264763/aotiqnmk0aii199nnbnz.png)',
+    backgroundRepeat: 'no-repeat',
+    backgroundColor:
+      theme.palette.type === 'light' ? theme.palette.grey[50] : theme.palette.grey[900],
+    backgroundSize: 'cover',
+    backgroundPosition: 'center',
+  },
+  paper: {
+    margin: theme.spacing(8, 4),
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+  },
+  avatar: {
+    margin: theme.spacing(1),
+    backgroundColor: theme.palette.secondary.main,
+  },
+  form: {
+    width: '100%', // Fix IE 11 issue.
+    marginTop: theme.spacing(1),
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2),
   },
 }));
 
 export default function Login() {
+  const classes = useStyles();
+  const [value, setValue] = React.useState(0);
   const [name,setName] = useState("");
   const [email,setEmail] = useState("");
   const[url,setUrl]= useState("");
   const [isLoggedIn, setIsLoggedIn] = useState(false);
   const[isEntity, setIsEntity]= useState(false);
-
-  const classes = useStyles();
-  const [value, setValue] = React.useState(0);
   const handleChange = (event, newValue) => {
     setValue(newValue);
   };
-
 
   const responseGoogleTrue = (response) =>{
     setName(response.profileObj.name);
@@ -82,6 +118,7 @@ export default function Login() {
     alert("No está registrado");
   };
 
+
   const handleOnClickClient = () => {
     if (document.getElementById("email").value === ""
     || document.getElementById("contrasenaCliente").value === ""
@@ -96,6 +133,7 @@ export default function Login() {
 
     }
     };
+  
 
   const handleOnClickEntity=() =>{
     if (document.getElementById("emailEmpresa").value === ""
@@ -108,33 +146,7 @@ export default function Login() {
     }
    };
 
-
-  const loginEntity = (entity) => {
-  
-
-    fetch('https://salesbox-alpha-backend.herokuapp.com/api/entity/user/'+entity.email+'/'+entity.password,{
-      method: 'GET'
-    }).then(function (response) {
-      if (response.ok) {
-        response.json().then(function (res) {
-          localStorage.setItem('isAdmin',true);
-          localStorage.setItem('nameEntity',res.name);
-          localStorage.setItem('entity', JSON.stringify(res));
-          localStorage.setItem('isLoggedIn',true);
-          setIsLoggedIn(true);
-        })
-      } else {
-        console.log("");
-        alert("Usuario o Contraseña Incorrecto");
-      }
-    }).catch(function (error) {
-      console.log("Bad petition:" + error.message);
-     
-    });
-
-  }
-
-  const loginClient = (client)=>{
+   const loginClient = (client)=>{
     fetch('https://salesbox-alpha-backend.herokuapp.com/clients/email/'+client.email,{
     //fetch('http://localhost:8080/clients/email/'+client.email,{
       method: 'GET'
@@ -167,124 +179,174 @@ export default function Login() {
     });
   }
 
+   const loginEntity = (entity) => {
+  
+
+    fetch('https://salesbox-alpha-backend.herokuapp.com/api/entity/user/'+entity.email+'/'+entity.password,{
+      method: 'GET'
+    }).then(function (response) {
+      if (response.ok) {
+        response.json().then(function (res) {
+          localStorage.setItem('isAdmin',true);
+          localStorage.setItem('nameEntity',res.name);
+          localStorage.setItem('entity', JSON.stringify(res));
+          localStorage.setItem('isLoggedIn',true);
+          setIsLoggedIn(true);
+        })
+      } else {
+        console.log("");
+        alert("Usuario o Contraseña Incorrecto");
+      }
+    }).catch(function (error) {
+      console.log("Bad petition:" + error.message);
+     
+    });
+
+  }
 
 
   return (
+  
+   
+    <Grid container component="main" className={classes.root}>
+      <CssBaseline />
+      <Grid item xs={false} sm={4} md={7} className={classes.image} />
+      <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
+        <div align="center" className={classes.paper}>
+    
+          <AppBar position="static" style={{ width: '600px', margin: '0 auto', background: '#D1B76C'}}>
+            <Tabs value={value} onChange={handleChange} aria-label="simple tabs example" >
+              <Tab style={{width:'50 auto'}} label={<span style={{ color: '#020202'  }}><FaceIcon></FaceIcon>Cliente</span>} {...a11yProps(0)} />
+              <Tab label={<span style={{ color: '#020202' }}><HomeWorkIcon></HomeWorkIcon>Empresa</span>} {...a11yProps(1)} />
+            </Tabs>
+          </AppBar>
+          {isLoggedIn && localStorage.getItem('isAdmin') ?
+            <div>
+              <Route>
+                <Redirect to={{ pathname: '/Dashboard', state: { isEntity: true } }}>
+                </Redirect>
+              </Route>
+            </div>
+            :
+            null
+          }
 
-
-    <div className={classes.root} style={{ width: '480px', margin: '0 auto' }}>
-      <AppBar position="static" style={{ width: '480px', margin: '0 auto', background: 'transparent' }}>
-        <Tabs value={value} onChange={handleChange} aria-label="simple tabs example" >
-          <Tab label={<span style={{ color: 'black' }}>Empresa</span>} {...a11yProps(0)} />
-          <Tab label={<span style={{ color: 'black' }}>Cliente</span>} {...a11yProps(1)} />
-        </Tabs>
-      </AppBar>
-
-      {isLoggedIn && localStorage.getItem('isAdmin')?
-          <div>
-          <Route>
-            <Redirect to={{pathname:'/Dashboard',state:{isEntity:true}}}>
-            </Redirect>
-          </Route>
-          </div>
-          :
-          null
-      }
-      <TabPanel value={value} index={0} >
-
-
-        <img src={logo} alt="logo" style={{ height: '150px', position: 'relative', top: '0px', right: '-100px' }} />
-
-
-
-        <input
-          id="emailEmpresa"
-          style={{ width: '400px', margin: '0 auto' }}
-          type="email"
-          className="form-controlj textbox-dgj"
-          placeholder="Email"
-
-
-        />
-        <br />
-        <br />
+      <TabPanel value={value} index={0}>
+      
+      
+     
+           
+            <br />
+            <br />
         
-        <input
-          id="contrasenaEmpresa"
-          style={{ width: '400px', margin: '0 auto' }}
-          type="Password"
-          className="form-controlj textbox-dgj"
-          placeholder="Contraseña"
-
-        />
-        <br />
-        <br />
-        <button className="myButton2" onClick={handleOnClickEntity} style={{ position: 'relative', top: '0px', right: '-140px' }}>
-         Iniciar  Sesión
-          </button>
-        <br>
-        </br>
-        <br></br>
-
-          <Link to="/Register"  style={{color:"grey" ,position: 'relative', top: '0px', right: '-130px' }}>Haz click para registrarte</Link>
-      </TabPanel>
-      <TabPanel value={value} index={1}>
-        <img src={logo} style={{ height: '150px', position: 'relative', top: '0px', right: '-100px' }} />
-
-        
-        <input
-          id="email"
-          style={{ width: '400px', margin: '0 auto' }}
-          type="email"
-          className="form-controlj textbox-dgj"
-          placeholder="Email"
+      <Input
+        position="static" 
+        id="email"
+        style={{ height: '40px',width: '500px'}}
+        type="email"
+        className="form-controlj textbox-dgj"
+        placeholder="Email"
 
 
 
-        />
-        <br />
-        <br />
-        <input
-          id="contrasenaCliente"
-          style={{ width: '400px', margin: '0 auto' }}
-          type="Password"
-          className="form-controlj textbox-dgj"
-          placeholder="Contraseña"
+      />
+      <br />
+            <br />
+            <br />
+            <br />
+      <Input
+        id="contrasenaCliente"
+        style={{ height: '40px', width: '500px', margin: '0 auto' }}
+        type="Password"
+        className="form-controlj textbox-dgj"
+        placeholder="Contraseña"
 
 
 
-        />
-        <br />
-        <br />
-        <button className="myButton2" onClick={handleOnClickClient} style={{ position: 'relative', top: '0px', right: '-140px' }}>
-          Iniciar Sesión
-          </button>
-        <br>
-        </br>
-        <br>
-        </br>
-        <Link to="/Register"  style={{color:"grey" ,position: 'relative', top: '0px', right: '-130px' }}>Haz click para registrarte</Link>
-        <br>
-        </br>
-        <br>
-        </br>
-        <br>
-        </br>
-          <GoogleLogin 
+      />
+      <br />
+            <br />
+            <br />
+            <br />
+      <button className="myButton2" onClick={handleOnClickClient} style={{ position: 'relative', top: '0px' }}>
+        Iniciar Sesión
+        </button>
+      <br>
+      </br>
+      <br>
+      </br>
+      <GoogleLogin 
             
             clientId="262410189500-9m36t0v9h1fuat5chk5ft7ttdlp9quk8.apps.googleusercontent.com"
             onSuccess={responseGoogleTrue}
             onFailure={responseGoogleFalse}
             cookiePolicy={'single_host_origin'}
-          
-           
-
-
         />
+      <br>
+      </br>
+      <br>
+      </br>
+      <Link to="/Register"  style={{color:"grey" ,position: 'relative' }}>Haz click para registrarte</Link>
+      <br>
+      </br>
+      <br>
+      </br>
+      <br>
+      </br>
+       
 
-      </TabPanel>
+    </TabPanel>
 
-    </div>
+          <TabPanel value={value} index={1} >
 
+
+           
+            <br />
+            <br />
+            <Input
+              id="emailEmpresa"
+              style={{  height: '40px',width: '500px', margin: '0 auto' }}
+              type="email"
+              className="form-controlj textbox-dgj"
+              placeholder="Email"
+
+
+            />
+            <br />
+            <br />
+            <br />
+            <br />
+
+            <Input
+              id="contrasenaEmpresa"
+              style={{  height: '40px', width: '500px', margin: '0 auto' }}
+              type="Password"
+              className="form-controlj textbox-dgj"
+              placeholder="Contraseña"
+
+            />
+            <br />
+            <br />
+            <br />
+            <br />
+            <button className="myButton2" onClick={handleOnClickEntity} style={{ position: 'relative', top: '0px'}}>
+              Iniciar  Sesión
+  </button>
+            <br>
+            </br>
+            <br></br>
+
+            <Link to="/Register" style={{ color: "grey", position: 'relative'}}>Haz click para registrarte</Link>
+          </TabPanel>
+
+         
+          <form className={classes.form} noValidate>
+
+      
+          </form>
+        </div>
+      </Grid>
+    </Grid>
+ 
   );
 }
